81756dd6ab299640a8f85344b1b609f389bd7afa
Kidus Worku <kidusmik@gmail.com>
2022-01-11 08:41:35 +0000

    Modify sort.h

--- sort.h
+++ sort.h
@@ -29,5 +29,7 @@ void quick_sort(int *array, size_t size);
 void quick_sort_main(int *array, int init, int final, size_t size);
 void shell_sort_main(int *array, size_t size, size_t gap, size_t index);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_node(listint_t **list, listint_t *node);
 
 #endif /* SORT_H */
